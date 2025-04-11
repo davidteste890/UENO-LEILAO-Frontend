c81286ccb99393eb5075232ab707b041618e3260
Your Name <you@example.com>
2025-04-11 02:23:26 -0300

    deploy: configura app.py para execução no Render

--- UENO-LEILAO-backend/app.py
+++ UENO-LEILAO-backend/app.py
@@ -16,4 +16,4 @@ from rotas import *
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Cria o banco e as tabelas se não existirem
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
